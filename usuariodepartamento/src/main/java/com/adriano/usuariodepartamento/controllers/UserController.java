f9e0025646807b990905fe0a37c42038213a8c34
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-10 15:13:25 -0300

    Composição para acesso ao UserRepository

--- usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/controllers/UserController.java
+++ usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/controllers/UserController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adriano.usuariodepartamento.entities.User;
+import com.adriano.usuariodepartamento.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	private UserRepository repository;
 	public List<User> findAll(){
 		
 	}
