2450e610fdc8279954647d9683cf058f7b2eb4ce
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-10 15:10:16 -0300

    Método para buscar todos usuários do banco de dados

--- usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/controllers/UserController.java
+++ usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.adriano.usuariodepartamento.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.adriano.usuariodepartamento.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	public List<User> findAll(){
+		
+	}
 }
