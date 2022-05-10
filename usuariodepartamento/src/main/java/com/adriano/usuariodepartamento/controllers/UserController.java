6c221692447d15a0a9b2a5ffca94e051e25b69bb
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-10 15:24:38 -0300

    GetMapping feito para requisição web

--- usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/controllers/UserController.java
+++ usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.adriano.usuariodepartamento.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
 	@Autowired
 	private UserRepository repository;
 
+	@GetMapping
 	public List<User> findAll() {
 		List<User> result = repository.findAll();
 		return result;
