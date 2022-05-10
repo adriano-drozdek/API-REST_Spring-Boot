71d9b09f722f1fec9cca5e863717a7e671b85966
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-10 14:19:46 -0300

    Configurado nome de chave estrangeira para o banco de dados

--- usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/entities/User.java
+++ usuariodepartamento/src/main/java/com/adriano/usuariodepartamento/entities/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -18,6 +19,7 @@ public class User {
 	private String email;
 
 	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 
 	public User() {
